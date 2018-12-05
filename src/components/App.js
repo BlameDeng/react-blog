7568e8c9e896f646f5a33971b2d6a3916a9acd2e
BlameDeng <476961779@qq.com>
2018-12-05 13:46:36 +0800

    route

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
 import '../style/App.scss'
+
+function Home() {
+  return (
+    <div>
+      <h2> Home页面 </h2>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h2> About页面 </h2>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/">About</Link>
-          </li>
-        </ul>
+        <div>
+          <ul>
+            <li>
+              <Link to="/"> Home </Link>
+            </li>
+            <li>
+              <Link to="/about"> About </Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+        </div>
       </Router>
     )
   }
